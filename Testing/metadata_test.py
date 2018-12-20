375e39f6b890af9e13cce451a570fd86d29ece2b
Andrew Growney <ajgrowney@gmail.com>
2018-12-20 11:20:45 -0600

    Update: removed old testing files

--- Testing/metadata_test.py
+++
@@ -1,9 +0,0 @@
-test_input = ["Kendrick-lamar/To-pimp-a-butterfly",
-              "Kendrick-lamar/Good-kid-m-a-a-d-city", 
-              "Logic/Under-pressure"
-]
-test_output = [
-    {"title": "To Pimp a Butterfly", "year": 2015},
-    {"title": "good kid, m.A.A.d city", "year": 2012},
-    {"title": "Under Pressure", "year": 2014}
-]
